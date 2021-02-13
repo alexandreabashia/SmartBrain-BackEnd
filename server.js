f9384452c526ef43006c5814cced8379680a5ce0
unknown <alexandreabashia@gmail.com>
2021-02-14 00:08:24 +0400

    adding db parameters for heroku

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ var cors = require('cors');
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: '123456',
-        database: 'smartbrain'
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
